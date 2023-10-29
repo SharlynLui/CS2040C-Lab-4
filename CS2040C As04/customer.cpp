6f605d3cbb2c7a760ca96cd07277a95245f3f4e3
SharlynLui <sharlynluitm@gmail.com>
2023-10-29 12:54:42 +0800

    Updated Task 2
    Jump to the next event after everyone is inserted

--- CS2040C As04/customer.cpp
+++ CS2040C As04/customer.cpp
@@ -9,17 +9,6 @@ bool Customer::operator>(const Customer& c) {
 	return comparisonWay ? processing_time < c.processing_time : arrival_time < c.arrival_time; // a customer is "greater" if his time is shorter
 };
 
-bool isSame(Customer a, Customer b) {
-	//assuming all customers arrive at different timings
-	if (a.AT() > b.AT()) {
-		return false;
-	}
-	if (b.AT() > a.AT()) {
-		return false;
-	}
-	return true;
-};
-
 void customerQueueTest(int n_cust) { //n_cust is 10 for the test case
 	int current_time = 0;
 	int totalWaitingTime = 0;
@@ -43,79 +32,48 @@ void customerQueueTest(int n_cust) { //n_cust is 10 for the test case
 	}
 	cout << endl;
 
-	for (int round = 0; round<1; round++) { //chnange back to round 2 when done
+	for (int round = 0; round<2; round++) {
 		cout << endl << endl;
 		cout << "Test Round " << round + 1 << endl;
 		cout << (round == 0 ? "First come first serve" : "Customer with the least PT served first") << endl;
 		comparisonWay = round; //round 0 is FIFO, round 1 compares the PT
 		current_time = 0;
 		totalWaitingTime = 0;
-		index = 0;
-		PT_left = 0;
+		index = 0; //keep track of how many customers have been served
+		PT_left = 0; //processing time left for current customer
 
-		while (index < n_cust) {
-			//customer will only arrive in the first 10 iteration of the loop for example
-			if (current_time < n_cust) {
+		while (index < n_cust) { //exit loop when all the customers have been served already
+			if (current_time < n_cust) { //Customer will only arrive in the first n_cust min according to customer behaviour
 				cout << "Customer arrives at time " << custList[current_time].AT() << " with PT=" << custList[current_time].PT() << endl;
-			}
-			if (queue.empty() && PT_left < 1) {
-				cout << "Customer arrives when no one is waiting" << endl;
-			}
-			if (current_time < n_cust) {
+				if (queue.empty() && PT_left < 1) {
+					//no one is waiting when the queue is empty and the server is not busy
+					cout << "Customer arrives when no one is waiting" << endl;
+				}
 				queue.insert(custList[current_time]);
 			}
-			if (PT_left < 1) {
+			if (PT_left < 1) { //server is done serving the customer and ready to serve the next one
 				current_cust = queue.extractMax();
-				PT_left = current_cust.PT();
-				index++;
+				PT_left = current_cust.PT(); //processing time resets with new customer's PT
+				index++; //previous customer is served and dequeued, serving the next one
 				WT = current_time - current_cust.AT();
 				totalWaitingTime += WT;
 				cout << "Customer is served at time " << current_time << " with AT=" << current_cust.AT() << " and PT=" << current_cust.PT() << " after waiting for "
 					<< WT << " min." << endl;
 			}
-			PT_left--;
-			current_time++;
+			if (current_time < n_cust) {
+				//every iternation of the loop is 1 min if there are still customers arriving
+				PT_left--;
+				current_time++;
+			}
+			else {
+				//once all the customers have arrived and inserted into the queue, jump to the next event
+				current_time += PT_left;
+				PT_left = 0;
+			}
 		}
 		cout << "Total Waiting Time: " << totalWaitingTime << endl;
 		cout << "Average Waiting Time: " << totalWaitingTime / (float)n_cust << endl;
 	}
 
-
 	delete[] custList;
 }
-
-/*		//check if <= is correct exit condition
-		while (index <= n_cust) {
-			// you should change all of the code here inside
-			//customer will only arrive in the first 10 iteration of the loop for example
-			if (current_time < n_cust) {
-				cout << "Customer arrives at time " << custList[current_time].AT() << " with PT=" << custList[current_time].PT() << endl;
-			} 
-			if (queue.empty()) {
-				cout << "Customer arrives when no one is waiting" << endl;
-			}
-			if (current_time < n_cust) {
-				queue.insert(custList[index]);
-			}
-			if (isSame(queue.peekMax(), custList[index])) {
-				if (printedserve == 0) {
-					WT = current_time - current_cust.AT();
-					totalWaitingTime += WT;
-					cout << "Customer is served at time " << current_time << " with AT=" << current_cust.AT() << " and PT=" << current_cust.PT() << " after waiting for "
-						<< WT << " min." << endl;
-					PT_left = current_cust.PT();
-					printedserve = 1;
-				}
-				PT_left--;
-			}
-			if (PT_left == 0) {
-				queue.deleteItem(current_cust);
-				printedserve = 0;
-				index++;
-				current_cust = queue.peekMax();
-			}
-			current_time++;
-		}
-		cout << "Total Waiting Time: " << totalWaitingTime << endl;
-		cout << "Average Waiting Time: " << totalWaitingTime / (float)n_cust << endl;
-		//round++;*/
